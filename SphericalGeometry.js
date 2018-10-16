610062989f5d62e1bd1dfa71a4598a6ccc8936d7
Philippe MEYER <p.meyer@mappingcontrol.com>
2018-10-16 14:28:09 +0200

    new version with custom rotation and network pattern around the sphere

--- SphericalGeometry.js
+++ SphericalGeometry.js
@@ -9,7 +9,7 @@
 
 var globals = {
 	r : 140,
-	definition : 90,
+	definition : 24,
 	starNr : 75,
 	globe : [],
 	offset : 0,
@@ -38,6 +38,8 @@ var globals = {
 	]
 };
 
+var startBeam,endBeam;
+
  function setup(){
   var cnv = createCanvas(globals.w,globals.h,WEBGL);
   cnv.parent('demoZone');
@@ -45,9 +47,9 @@ var globals = {
 	 globals.auto = !globals.auto;
   });
   
-  globals.sunLight = createVector(-HALF_PI,0.5,HALF_PI+0.1);
+  globals.sunLight = createVector(-HALF_PI,0,HALF_PI);
   globals.lastRotationX = HALF_PI;
-  frameRate(30);
+ // frameRate(30);
   globals.startBg = createGraphics(globals.w,globals.h);
 
   globals.startBg.fill(245);
@@ -69,17 +71,11 @@ globals.startBg.stroke(255);
 globals.startBg.filter(ERODE,DILATE); // DILATE ,BLUR ,POSTERIZE,
 globals.startBg.ellipseMode(CENTER)
 globals.startBg.stroke("yellow");
- globals.startBg.translate(globals.w/2,globals.h/2);
- var scale = 1 - globals.sunLight.z;
- var startBeam = {x:globals.sunLight.x*globals.w/scale,y:globals.sunLight.y*globals.h/scale};
-  globals.startBg.ellipse(startBeam.x,startBeam.y,10 / scale,10 / scale);
- var endBeam = {x:0,y:0};
+startBeam = {x:globals.sunLight.x,y:globals.sunLight.y,z:globals.sunLight.z};
+endBeam = {x:0,y:0,z:0};
 
 
-  //globals.startBg.ellipse(endBeam.x,endBeam.y,10 / scale,10 / scale);
-  //globals.startBg.line(startBeam.x,startBeam.y,endBeam.x,endBeam.y);
 
-var frontier = globals.definition+1 / 2;
  globals.globe = [];
 	for(var i = 0; i < globals.definition+2; i++){
 		var lat = map(i,0,globals.definition,0, PI);
@@ -95,8 +91,16 @@ var frontier = globals.definition+1 / 2;
 			y = globals.r * y;
 			z = globals.r * z;
 			var pt = createVector(x,y,z);
-			
-			globals.globe[i].push({"point":pt,"normale":normale});
+			var isBeacon = (random() <= 0.08);
+			globals.globe[i].push({
+			"point":pt,
+			"normale":normale,
+			"beacon":{
+				"isBeacon" : isBeacon,
+				"beaconData":[],
+				"beaconMax": map(random(),0,1,10,25)
+				}
+			});
 		}
 	}
 	
@@ -112,8 +116,8 @@ var frontier = globals.definition+1 / 2;
  }
  globals.offset=0;
  
-var deltaZ = 0.002;
-var deltaX = 0.005;
+var deltaZ = 0.009;
+var deltaX = 0.1;
 var deltaY = 0;	
  function draw(){	 
   background(0);
@@ -121,7 +125,7 @@ var deltaY = 0;
   texture(globals.startBg);
   plane(globals.w,globals.h);
 
-
+line(startBeam.x,startBeam.y,startBeam.z,endBeam.x,endBeam.y,endBeam.z);
 
 	if(abs(globals.lastRotationZ) >= 0.3){
 		deltaZ*=-1;
@@ -147,12 +151,11 @@ var deltaY = 0;
 	}
 	
 	for(var i = 1; i < globals.definition+1; i++){
-		
-	
 		beginShape(TRIANGLE_STRIP);
 		for(var j = 0; j < globals.definition+2; j++){
+			var mainPt = globals.globe[i][j];
 			var color = {r:45,g:45,b:55};
-			var dot = scalarProduct(globals.globe[i][j].normale,globals.sunLight);
+			var dot = scalarProduct(mainPt.normale,globals.sunLight);
 			 if(dot >= 0){
 				 var lightStrength = round(dot*dot*30);
 				color.r += lightStrength;
@@ -166,14 +169,85 @@ var deltaY = 0;
 				color.b += lightStrength;
 			 } 
 			fill(color.r,color.g,color.b);
-			//stroke(color.r,color.g,color.b);
-			var v1 = globals.globe[i][j].point;	
+			var v1 = mainPt.point;	
 			vertex(v1.x,v1.y,v1.z);
 			var v2 = globals.globe[i-1][j].point;
 			vertex(v2.x,v2.y,v2.z);
+
 		}
 		endShape();
 	}
+	
+	for(var i = 1; i < globals.definition; i++){
+			for(var j = 0; j < globals.definition; j++){
+				var mainPt = globals.globe[i][j];{
+					if(mainPt.beacon.isBeacon){
+					var data = mainPt.beacon.beaconData;
+					var linesMax = mainPt.beacon.beaconMax;
+					var len = data.length;
+					if(len > linesMax || len == 0){
+						data.length = 0;
+						data.push({"i":i,"j":j});
+					}
+					
+					addNextPoint(data);
+					drawBeaconTrail(data);
+				
+				}
+			}
+			
+	}
+  }
+}
+
+ function drawBeaconTrail(data){
+	 		push();
+			stroke(255,240,80);
+			noFill();
+			for(var x= 0; x < data.length-1;x++){
+				var root = globals.globe[data[x].i][data[x].j].point;
+				var next = globals.globe[data[x+1].i][data[x+1].j].point;
+				line(root.x, root.y, root.z, next.x, next.y, next.z);
+			}		
+			pop();
+ }
+ 
+ function addNextPoint(data){
+	var len = data.length;
+
+	var root = data[len-1]; 
+	var i = root.i;
+	var j = root.j;
+	var nextI = i;
+	var nextJ = j;
+	var test4Directions = round(map(random(),0,1,1,4));
+	if(test4Directions== 1){
+		// top
+		nextI = i-1;
+		if (nextI < 0) nextI = globals.definition -1;
+	}else if(test4Directions== 2){
+		// right
+		var nextJ = j+1;
+		if (nextJ > globals.definition -1) nextJ = 0;
+	}else if(test4Directions== 3){
+		// bottom
+		nextI = i+1;
+		if (nextI > globals.definition -1) nextI = 0;
+	}else if(test4Directions== 4){
+		// left
+		var nextJ = j-1;
+		if (nextJ < 0) nextJ = globals.definition -1;
+	}
+ // TODO : check if the branch is already in the data array
+ var notFound = true;
+ data.forEach(function(d){
+	 if(d.i == nextI && d.j == nextJ){
+		 notFound = false;
+	 }
+ });
+ if(notFound){
+	data.push({"i":nextI,"j":nextJ});
+ }
  }
  
 function scalarProduct(a,b){
